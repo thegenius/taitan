11fde296e4f36390f8d2f3aea47ddcdbb1bf8cc6
root <soulww@163.com>
2024-07-08 16:38:22 +0800

    add json result

--- taitan/src/result.rs
+++ taitan/src/result.rs
@@ -1,6 +1,7 @@
 use crate::error::Error;
 use crate::response::{ApiFailure, ApiResponse, ApiSuccess, ApiSuccessOpt};
 pub type Result<T> = std::result::Result<T, Error>;
+use axum::response::Json;
 
 /* can not implement IntoResponse for Result
 impl<T> IntoResponse for Result<T> {
@@ -24,6 +25,18 @@ where
     }
 }
 
+impl<'a, T> From<Result<Json<T>>> for ApiResponse<'a, T>
+where
+    T: serde::Serialize,
+{
+    fn from(origin: Result<Json<T>>) -> Self {
+        match origin {
+            Ok(data) => ApiResponse::success(data.0),
+            Err(err) => ApiResponse::from(err),
+        }
+    }
+}
+
 impl<'a, T> From<Result<Option<T>>> for ApiResponse<'a, T>
 where
     T: serde::Serialize,
@@ -36,6 +49,18 @@ where
     }
 }
 
+impl<'a, T> From<Result<Json<Option<T>>>> for ApiResponse<'a, T>
+where
+    T: serde::Serialize,
+{
+    fn from(origin: Result<Json<Option<T>>>) -> Self {
+        match origin {
+            Ok(data) => ApiResponse::<T>::success_opt(data.0),
+            Err(err) => ApiResponse::from(err),
+        }
+    }
+}
+
 impl<'a, T> From<Result<Vec<T>>> for ApiResponse<'a, T>
 where
     T: serde::Serialize + Clone,
@@ -43,6 +68,18 @@ where
     fn from(origin: Result<Vec<T>>) -> Self {
         match origin {
             Ok(data) => ApiResponse::<T>::success_array(data),
+            Err(err) => ApiResponse::from(err),
+        }
+    }
+}
+
+impl<'a, T> From<Result<Json<Vec<T>>>> for ApiResponse<'a, T>
+where
+    T: serde::Serialize + Clone,
+{
+    fn from(origin: Result<Json<Vec<T>>>) -> Self {
+        match origin {
+            Ok(data) => ApiResponse::<T>::success_array(data.0),
             Err(err) => ApiResponse::from(err),
         }
     }
